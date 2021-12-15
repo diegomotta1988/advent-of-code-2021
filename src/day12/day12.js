1481364f50eca17ea24edf4b74f9d29fac09cc26
DiegoMotta <diegomottaalvarez@gmail.com>
2021-12-15 17:15:30 +0100

    day12-14

--- src/day12/day12.js
+++ src/day12/day12.js
@@ -1,24 +1,67 @@
-const RAW_INPUT_DEMO = `start-A
-start-b
-A-c
-A-b
-b-d
-A-end
-b-end`;
+import { RAW_INPUT } from './day12.data';
 
-const keyValues = {};
-
-const parsedInput = RAW_INPUT_DEMO.split('\n')
-  .map((row) => row.split('-'))
-  .map((item) => {
-    keyValues[item[0]]
-      ? keyValues[item[0]].push(item[1])
-      : (keyValues[item[0]] = [item[1]]);
-  });
-
-console.log(keyValues);
-
-const getPaths = () => {
-  for (let link of keyValues.start) {
+const graph = {};
+const parsedInput = RAW_INPUT.split('\n').map((row) => {
+  const [from, to] = row.split('-');
+  if (!graph[from]) {
+    graph[from] = [];
   }
+  if (!graph[to]) {
+    graph[to] = [];
+  }
+  graph[from].push(to);
+  graph[to].push(from);
+  return { from, to };
+});
+
+console.log(parsedInput);
+console.log(graph);
+
+const isSmallCave = (string) => {
+  return /[a-z]/.test(string);
 };
+
+const part1 = () => {
+  const depthFirstSearch = (node, visited = [], paths) => {
+    visited.push(node);
+    if (node === 'end') {
+      paths.push(visited.join`,`);
+      return visited;
+    }
+    for (const neighbor of graph[node]) {
+      if (isSmallCave(neighbor) && visited.includes(neighbor)) {
+        continue;
+      }
+      depthFirstSearch(neighbor, [...visited], paths);
+    }
+  };
+  const paths = [];
+  depthFirstSearch('start', [], paths);
+  console.log(paths.length);
+};
+
+const part2 = () => {
+  const depthFirstSearch = (node, visited = [], visitedTwiceAlready, paths) => {
+    visited.push(node);
+    if (node === 'end') {
+      paths.push(visited.join`,`);
+      return visited;
+    }
+    for (const neighbor of graph[node]) {
+      if (neighbor === 'start') continue;
+      if (isSmallCave(neighbor) && visited.includes(neighbor)) {
+        if (visitedTwiceAlready) {
+          continue;
+        }
+        depthFirstSearch(neighbor, [...visited], true, paths);
+      } else {
+        depthFirstSearch(neighbor, [...visited], visitedTwiceAlready, paths);
+      }
+    }
+  };
+  const paths = [];
+  depthFirstSearch('start', [], false, paths);
+  console.log(paths.length);
+};
+
+part2();
